6dbfba9f5208a07069d4d8f96a25b1c8dc9c1eca
Jrich45 <127894540+Jrich45@users.noreply.github.com>
2023-08-12 11:25:44 -0400

    Finished functions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 class Calculator {
     constructor(previousOperandandTextElement, currentOperandandTextElement) {
         this.previousOperandandTextElement = previousOperandandTextElement
@@ -15,6 +20,7 @@ class Calculator {
     }
 
     delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
 
     }
 
@@ -27,19 +33,75 @@ class Calculator {
 
     chooseOperation(operation) {
 
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+
     }
 
     compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch(this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            
+            case '-':
+                computation = prev - current
+                break
+            
+            case '*':
+                computation = prev * current
+                break
+            
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+       
 
     }
 
     updateDisplay() {
-        this.currentOperandandTextElement.innerText = this.currentOperand
-
+        this.currentOperandandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        }
+        this.previousOperandandTextElement = this.previousOperand
     }
 }
-
-
 
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -58,6 +120,10 @@ const currentOperandandTextElement = document.querySelector('[data-current-opera
 
 const calculator = new Calculator(previousOperandandTextElement, currentOperandandTextElement)
 
+
+
+
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
@@ -65,4 +131,30 @@ numberButtons.forEach(button => {
 
     })
 })
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+
 
